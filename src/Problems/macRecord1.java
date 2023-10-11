8917e83d260f8ba089c84162d495acd921838fd6
Seebo Beebo <me.is.seb.boy@gmail.com>
2023-10-11 02:33:27 -0700

    Did some work on macRecord1

--- src/Problems/macRecord1.java
+++ src/Problems/macRecord1.java
@@ -1,2 +1,6 @@
-package Problems;public class macRecord1 {
+package Problems;
+public class macRecord1 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
 }
