695b740c039e894a98630e235affb5cad53af65c
Seebo Beebo <me.is.seb.boy@gmail.com>
2023-10-12 00:25:09 -0700

    file upload for transfer to macbook

--- src/Learn/LearnJavaStackMehtond.java
+++
@@ -1,4 +0,0 @@
-package Learn;
-
-public class LearnJavaStackMehtond {
-}
---
+++ src/Learn/LearnJavaStackMethod.java
@@ -0,0 +1,21 @@
+package Learn;
+import java.util.*;
+
+public class LearnJavaStackMehtond {
+    public static void main(String[] args){
+
+        Stack<Character> stack = new Stack<>();
+        stack.push('a');
+        stack.push('b');
+
+        //Pop method: remove and retrieve the element at the top of the stack, you use the pop method
+        char topElement = stack.pop();
+
+        //Peek: To view the element at the top of the stack without removing it, you use the peek method.
+        topElement = stack.peek();
+
+        //Empty check: To check if the stack is empty, you can use the isEmpty method
+        boolean isEmpty = stack.isEmpty();
+
+    }
+}
