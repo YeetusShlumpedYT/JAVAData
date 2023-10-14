165c8594db898a06482a705e9ac6aad07507d0f4
Seebo Beebo <sebastianmembreno@sebastians-air.mynetworksettings.com>
2023-10-13 21:03:46 -0700

    Updated files

---
+++ src/Problems/SubarraySumEqualsK.java
@@ -0,0 +1,13 @@
+package Problems;
+
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    private static int subarraySumEqualsK(int[] nums, int k){
+        List<Integer> subarray = new ArrayList<>();
+        for(int i = 0;i< nums.length;i++){
+
+        }
+        return subarray.size();
+    }
+}
